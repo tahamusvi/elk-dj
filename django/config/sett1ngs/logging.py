4a7781dff3fe90290c75ec40fc06b0e805dacb1d
Taha <TahaM8000@gmail.com>
2025-05-12 19:53:40 +0330

    django logs

--- django/config/sett1ngs/logging.py
+++ django/config/sett1ngs/logging.py
@@ -1,24 +1,23 @@
+import os
+
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': '{levelname} {asctime} {module} {message}',
-            'style': '{',
-        },
-    },
     'handlers': {
         'file': {
-            'level': 'INFO',
+            'level': 'DEBUG',
             'class': 'logging.FileHandler',
-            'filename': '/logs/django.log',
-            'formatter': 'verbose',
+            'filename': os.getenv('DJANGO_LOG_FILE', 'logs/django.log'),
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['file'],
-            'level': 'INFO',
+            'handlers': ['file', 'console'],
+            'level': 'DEBUG',
             'propagate': True,
         },
     },
